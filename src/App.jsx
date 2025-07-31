26e855bdbea04a3bce494f01c37f1351a44a1826
chaithanyaponnusamy02 <chaithanya2646@gmail.com>
2025-07-31 22:48:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  const [todo,setTodo] = useState("");
+  const [todolist,setTodoList] = useState([]);
+
+  const handleAdd = () => {
+    if (todo.trim() === "") return;
+    setTodoList([...todolist, { id: Date.now(), text: todo, completed: false }]);
+    setTodo("");
+  };
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <div className="input-container">
+        <input type="text" placeholder='Add a new Task' value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button className='add-button' onClick={handleAdd}>Add</button>
+      </div>
+      <ul className='todo-list'>
+        {todolist.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
