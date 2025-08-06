3f83db9e3ac4b389d41505c4021111a407847818
chaithanyaponnusamy02 <chaithanya2646@gmail.com>
2025-08-06 23:27:24 +0530

    Styles

--- src/login.jsx
+++ src/login.jsx
@@ -8,14 +8,7 @@ const login = () => {
         <input type="password" placeholder='Password' />
         <button type='submit'>Login</button>
         <p>Don't have an account? <Link to='/register'>Register</Link></p>
-        <p>Forgot password? <Link to='/forgot-password'>Reset Password</Link></p>
-        <p>Or login with</p>
-        <button>Google</button>
-        <button>Facebook</button>
-        <button>Apple</button>
-        <p>By continuing, you agree to our <Link to='/terms'>Terms of Service</Link> and <Link to='/privacy'>Privacy Policy</Link></p>
         <p>Already have an account? <Link to='/login'>Login</Link></p>
-        <p>Don't have an account? <Link to='/register'>Register</Link></p>
     </form>
   )
 }
